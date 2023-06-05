2179597ce5d642e391f2ce0ce18caa7809b77d71
ha-spring <spring4boot@gmail.com>
2023-06-05 10:15:48 +0000

    Converting Strings into Errors

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub struct Config {
 fn parse_positive_int(val: &str) -> MyResult<usize> {
     match val.parse() {
         Ok(n) if n > 0 => Ok(n),
-        _ => Err(From::from(val)),
+        _ => Err(val.into()),
     }
 }
 
@@ -42,25 +42,28 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .about("Rust head")
         .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Number of lines")
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .takes_value(true)
+                .conflicts_with("lines")
+                .help("Number of bytes"),
+        )
+        .arg(
             Arg::with_name("files")
                 .value_name("FILE")
                 .help("Input file(s)")
                 .multiple(true)
                 .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("lines")
-                .short("n")
-                .long("number")
-                .help("Number lines")
-                .default_value("10")
-                .conflicts_with("bytes"),
-        )
-        .arg(
-            Arg::with_name("bytes")
-                .short("b")
-                .long("bytes")
-                .help("Number bytes"),
         )
         .get_matches();
 
